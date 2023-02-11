76e1f8e9eea85c85c0a26b48e48335ebff1278da
Yurii <iyureg@gmail.com>
2023-02-11 15:31:29 +0300

    основные типы данных TypeScript

---
+++ innerTS/app.js
@@ -0,0 +1,4 @@
+var str = "it works";
+var num = 123;
+var bool = true;
+console.log(str, num, bool);
---
+++ innerTS/app.ts
@@ -0,0 +1,6 @@
+let str: string = "it works"; // строковый тип данных
+
+let num: number = 123; // числовой тип данных
+
+let bool: boolean = true; // логический тип данных
+console.log(str, num, bool);
