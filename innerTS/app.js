d80d3eb5b035ba531bb8d06f80d8126cca219f23
Yurii <iyureg@gmail.com>
2023-02-11 16:02:48 +0300

    изменение значений и типы данных

--- innerTS/app.js
+++ innerTS/app.js
@@ -1,4 +1,11 @@
-var str = "it works";
-var num = 123;
-var bool = true;
+var str = "it works"; // строковый тип данных
+str = "it works too";
+// str = 123 ошибка, попытка записи числа в строковый тип
+var num = 123; // числовой тип данных
+num = 456;
+// num = "goes't work";
+var bool = true; // логический тип данных (два значения true или false)
+bool = false;
+//bool = 134; ошибка
+//bool = "string";  ошибка
 console.log(str, num, bool);
--- innerTS/app.ts
+++ innerTS/app.ts
@@ -1,6 +1,15 @@
-let str: string = "it works"; // строковый тип данных
+var str: string = "it works"; // строковый тип данных
 
-let num: number = 123; // числовой тип данных
+str = "it works too";
+// str = 123 ошибка, попытка записи числа в строковый тип
 
-let bool: boolean = true; // логический тип данных
+var num: number = 123; // числовой тип данных
+num = 456;
+// num = "goes't work";
+
+var bool: boolean = true; // логический тип данных (два значения true или false)
+bool = false;
+//bool = 134; ошибка
+//bool = "string";  ошибка
+
 console.log(str, num, bool);
